fa723a80e68474d5e646b8e53b09aadd884b7f0b
Your Name <you@example.com>
2023-04-11 11:29:07 -0300

    primeiro

---
+++ src/data/movies.js
@@ -0,0 +1,67 @@
+import react from "react";
+
+
+
+const DATA =
+[
+    {
+        id: '01',
+        nome: 'Patrulha canina',
+        nota: '8.8',
+        imagem: '1.jpg'
+    },
+    {
+        id:'02',
+        nome: 'Velozes e Furiosos',
+        nota:'7.3',
+        imagem:'2.jpg'
+    },
+    {
+        id:'03',
+        nome: 'Star Wars Guerra de clones',
+        nota:'9.7',
+        imagem:'3.jpg'  
+    },
+    {
+        id:'04',
+        nome: 'Vingadores',
+        nota:'10',
+        imagem:'4.jpg'
+    },
+    {
+        id:'05',
+        nome: 'Resident evil',
+        nota:'7.4',
+        imagem:'5.jpg'
+    },
+    {
+        id:'06',
+        nome: 'Homen Aranha',
+        nota:'9.8',
+        imagem:'6.jpg'
+    },
+    {
+        id:'07',
+        nome: 'Capitão America',
+        nota:'6.1',
+        imagem:'7.jpg'
+    },
+    {
+        id:'08',
+        nome: 'The witcher a origem do lobo',
+        nota:'9.8',
+        imagem:'8.jpg'
+    },
+    {
+        id:'09',
+        nome: 'Guerra mundial Z',
+        nota:'10',
+        imagem:'9.jpg'
+    },
+    {
+        id:'10',
+        nome: 'Os simpsons',
+        nota:'8.8',
+        imagem:'10.jpg'
+    }
+]
